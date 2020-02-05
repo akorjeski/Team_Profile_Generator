b1b3fa1c49e27a0aeb8314eb9bc35b0a475699e7
Alex Korjeski <akorjeski@gmail.com>
2020-02-05 18:17:52 -0500

    tried tests, couldnt get one to work

--- app.js
+++ app.js
@@ -138,7 +138,7 @@ function generateHTML() {
         <style>
             header {
                 position: relative;
-                background-color: red;
+                background-color: green;
                 color: white;
                 width: 100%;
                 height: 100px;
